559959f945980b88f1c82c05fceb435996158047
Sonu Mahto <143511693+sonu-mern@users.noreply.github.com>
2024-07-18 07:06:01 +0000

    bug resolve

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "CopyToClipboard", {
+  enumerable: true,
+  get: function get() {
+    return _CopyToClipboard["default"];
+  }
+});
+var _CopyToClipboard = _interopRequireDefault(require("./CopyToClipboard"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 
-export { default as CopyToClipboard } from './CopyToClipboard';
+import CopyToClipboard from './CopyToClipboard';
+export { CopyToClipboard };
